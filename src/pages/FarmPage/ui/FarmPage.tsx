9a92f3a3cd0dd831dc094e87d2b6aa9d87e973e4
dolgoshein-na <dolgosheinna@intech.rshb.ru>
2023-11-04 18:37:17 +0300

    feat(farm): add svg map of the game

---
+++ src/pages/FarmPage/ui/FarmPage.tsx
@@ -0,0 +1,12 @@
+import FarmMap from "shared/assets/images/farm/map.svg";
+import cls from "./FarmPage.module.scss";
+
+const FarmPage = () => {
+  return (
+    <div className={cls.FarmPage}>
+      <FarmMap />
+    </div>
+  );
+};
+
+export default FarmPage;
--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -3,44 +3,52 @@ import { MainPage } from 'pages/MainPage';
 import { AboutPage } from 'pages/AboutPage';
 import { NotFoundPage } from 'pages/NotFoundPage';
 import { ProfilePage } from 'pages/ProfilePage';
+import {FarmPage} from "pages/FarmPage";
 
 export type AppRoutesProps = RouteProps & {
     authOnly?: boolean;
 }
 
 export enum AppRoutes {
-    MAIN = 'main',
-    ABOUT = 'about',
-    PROFILE = 'profile',
-    // last
-    NOT_FOUND = 'not_found',
+  MAIN = "main",
+  ABOUT = "about",
+  PROFILE = "profile",
+  MY_FARM = "farm",
+  // last
+  NOT_FOUND = "not_found",
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
-    [AppRoutes.MAIN]: '/',
-    [AppRoutes.ABOUT]: '/about',
-    [AppRoutes.PROFILE]: '/profile',
-    // последний
-    [AppRoutes.NOT_FOUND]: '*',
+  [AppRoutes.MAIN]: "/",
+  [AppRoutes.ABOUT]: "/about",
+  [AppRoutes.PROFILE]: "/profile",
+  [AppRoutes.MY_FARM]: "/farm",
+  // последний
+  [AppRoutes.NOT_FOUND]: "*",
 };
 
 export const routeConfig: Record<AppRoutes, AppRoutesProps> = {
-    [AppRoutes.MAIN]: {
-        path: RoutePath.main,
-        element: <MainPage />,
-    },
-    [AppRoutes.ABOUT]: {
-        path: RoutePath.about,
-        element: <AboutPage />,
-    },
-    [AppRoutes.PROFILE]: {
-        path: RoutePath.profile,
-        element: <ProfilePage />,
-        authOnly: true,
-    },
-    // last
-    [AppRoutes.NOT_FOUND]: {
-        path: RoutePath.not_found,
-        element: <NotFoundPage />,
-    },
+  [AppRoutes.MAIN]: {
+    path: RoutePath.main,
+    element: <MainPage />,
+  },
+  [AppRoutes.ABOUT]: {
+    path: RoutePath.about,
+    element: <AboutPage />,
+  },
+  [AppRoutes.PROFILE]: {
+    path: RoutePath.profile,
+    element: <ProfilePage />,
+    authOnly: true,
+  },
+  [AppRoutes.MY_FARM]: {
+    path: RoutePath.farm,
+    element: <FarmPage />,
+    authOnly: true,
+  },
+  // last
+  [AppRoutes.NOT_FOUND]: {
+    path: RoutePath.not_found,
+    element: <NotFoundPage />,
+  },
 };
