86b57c205f8c915e4fea869575fca60011c3b046
vladimirova.a-a <vladimirovaaal@intech.rshb.ru>
2023-11-10 11:47:07 +0400

    fix(*): fix not found page

--- src/pages/NotFoundPage/ui/NotFoundPage.tsx
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -1,6 +1,7 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import cls from './NotFoundPage.module.scss';
+import {useEffect, useState} from "react";
 
 interface NotFoundPageProps {
     className?: string;
@@ -8,9 +9,18 @@ interface NotFoundPageProps {
 
 export const NotFoundPage = ({ className }: NotFoundPageProps) => {
     const { t } = useTranslation();
+
+    const [text, setText] = useState(null)
+
+    useEffect(() => {
+        setTimeout(() => {
+            setText(t('Страница не найдена'))
+        }, 1_500)
+    }, []);
+
     return (
         <div className={classNames(cls.NotFoundPage, {}, [className])}>
-            {t('Страница не найдена')}
+            {text}
         </div>
     );
 };
