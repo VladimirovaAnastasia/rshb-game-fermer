bacd54a343405e02c9299ab68b722f541aa3a6ee
dolgoshein-na <dolgosheinna@intech.rshb.ru>
2023-11-28 21:21:10 +0300

    feat(*): fix naming of app foolder

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,30 +1,30 @@
-import { Suspense, useEffect } from 'react';
-import { classNames } from 'shared/lib/classNames/classNames';
-import { useTheme } from 'app/providers/ThemeProvider';
-import { AppRouter } from 'app/providers/router';
-import { useDispatch } from 'react-redux';
-import { userActions } from 'entities/User';
-import { GameLayout } from 'widgets/GameLyaout';
+import { Suspense, useEffect } from "react";
+import { classNames } from "shared/lib/classNames/classNames";
+import { useTheme } from "app/providers/ThemeProvider";
+import { AppRouter } from "app/providers/AppRouter";
+import { useDispatch } from "react-redux";
+import { userActions } from "entities/User";
+import { GameLayout } from "widgets/GameLyaout";
 
 function App() {
-    const { theme } = useTheme();
-    const dispatch = useDispatch();
+  const { theme } = useTheme();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(userActions.initAuthData());
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(userActions.initAuthData());
+  }, [dispatch]);
 
-    return (
-        <div className={classNames('app', {}, [theme])}>
-            <Suspense fallback="">
-                <div className="content-page">
-                    <GameLayout>
-                        <AppRouter />
-                    </GameLayout>
-                </div>
-            </Suspense>
+  return (
+    <div className={classNames("app", {}, [theme])}>
+      <Suspense fallback="">
+        <div className="content-page">
+          <GameLayout>
+            <AppRouter />
+          </GameLayout>
         </div>
-    );
+      </Suspense>
+    </div>
+  );
 }
 
 export default App;
